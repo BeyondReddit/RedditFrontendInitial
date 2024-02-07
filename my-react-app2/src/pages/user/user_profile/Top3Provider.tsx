43acfb5da8efaff0420621cf2817b796c7b010c5
RabbitTank1012 <gtmdqyd233@gmail.com>
2024-02-06 20:41:18 -0500

    Fix Top3

--- my-react-app2/src/pages/user/user_profile/Top3Provider.tsx
+++ my-react-app2/src/pages/user/user_profile/Top3Provider.tsx
@@ -23,28 +23,51 @@ const Top3Provider = ({ children }: IProps) => {
         {postid:"65c198ba70823d3e2fea0bfb",title:"Dorland"},
         {postid:"65c198ba70823d3e2fea0bfb",title:"Star"}]);
 
+    // useEffect(() => {
+    //
+    //     const loadedTop3: Top3Item[] = [];
+    //     fetch('http://localhost:10010/posts/top')
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             console.log("Current data i is"+data);
+    //
+    //                 for (let i =0; i < data.length; i++){
+    //
+    //                     loadedTop3.push({postid:data[i].postid,title:data[i].title});
+    //                     console.log("Current data posttitle is"+data[i].title);
+    //                     setUsers(loadedTop3);
+    //
+    //                 }
+    //
+    //             }
+    //         );
+    //
+    // }, []);
+
     useEffect(() => {
-
-        const loadedTop3: Top3Item[] = [];
         fetch('http://localhost:10010/posts/top')
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("Current data i is"+data);
-
-                    for (let i =0; i < data.length; i++){
-
-                        loadedTop3.push({postid:data[i].postid,title:data[i].title});
-                        console.log("Current data posttitle is"+data[i].title);
-                        setUsers(loadedTop3);
-
-                    }
-
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch top 3 posts');
                 }
-            );
-
+                return response.json();
+            })
+            .then((data) => {
+                // Check if data is an object with 'data' property containing the array
+                if (data && data.data && Array.isArray(data.data)) {
+                    const loadedTop3: Top3Item[] = data.data.map((item: any) => ({
+                        postid: item.postId,
+                        title: item.title
+                    }));
+                    setUsers(loadedTop3);
+                } else {
+                    console.error('Unexpected data structure:', data);
+                }
+            })
+            .catch((error) => {
+                console.error('Error fetching top 3 posts:', error);
+            });
     }, []);
-
-
 
 
 
